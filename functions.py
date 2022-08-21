d3f17bca9d07ab8b556651373753fad0f09e3d0a
Tryd0g0lik <work80@mail.ru>
2022-08-21 14:10:07 +0700

    first commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+class Stack:
+  def __init__(self):
+    self.stack = []
+
+  def isEmpty(self):
+    if type(self.stack) is not None:
+      ...
+    else:
+      ...
+
+  def push(self):
+    self.stack.append(...)
+
+  def pop(self):
+    self.stack.pop()
+
+  def peek(self):
+    ...
+
+  def size(self):
+    ...
+
+
