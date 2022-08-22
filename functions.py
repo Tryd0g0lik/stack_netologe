50534a529e63b17656ddb678e701400d2f85d274
Tryd0g0lik <work80@mail.ru>
2022-08-22 10:14:10 +0700

    Basic level when to find `qual symbols '}]){([' and delete

--- functions.py
+++ functions.py
@@ -1,12 +1,73 @@
+import re
+
 class Stack:
-  def __init__(self):
+  def __init__(self, var_string : str):
+    self.var_string = var_string
     self.stack = []
+    self.verify = True
 
   def isEmpty(self):
-    if type(self.stack) is not None:
-      ...
+    symbol : str
+    time_symbol : str
+
+
+    if len(self.var_string) != 0:
+      var_string = reversed(self.var_string)
+      for symbol in var_string:
+        if symbol in "{([":
+          self.stack.append(symbol)
+
+        for time_symbol in "}])":
+
+          if symbol == "{" and time_symbol == "}":
+            # template = re.compile("[^.*]\{[^\[\{\(]*.*[^\}\]\)]*\}[^.*]")
+            template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\{{1}.{0,}\}{1})[^.*\}\]\)\{\(\[]{0,}")
+
+            old_str = (re.search(template, self.var_string).group(0))[1:-1]
+            truth = [truth for truth in old_str if truth in "}]){(["]
+            if truth != []:
+              print("One symbol '}]){([' found")
+              exit()
+
+            result = self.var_string.replace(str(old_str), "")
+            self.var_string = result
+            # template = old_str = result = None
+            print(self.var_string)
+          if symbol == "(" and time_symbol == ")":
+            # template = re.compile("[^.*]\([^\[\{\(]*.*[^\}\]\)]*\)[^.*]")
+            template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\({1}.{0,}\){1})[^.*\}\]\)\{\(\[]{0,}")
+
+            old_str = (re.search(template, self.var_string).group(0)[1:-1])
+            truth = [truth for truth in old_str if truth in "}]){(["]
+            if truth != []:
+              print("One symbol '}]){([' found")
+              exit()
+
+
+            result = self.var_string.replace(str(old_str), "")
+            self.var_string = result
+            # template = old_str = result = None
+            print(self.var_string)
+          if symbol == "[" and time_symbol == "]":
+            # template = re.compile("[^.*]\[[^\[\{\(]*.*[^\}\]\)]*\][^.*]")
+            # template = re.compile("[^.*]{0,}(\[[^\[\{\(]*.*[^\}\]\)]*\])[^.*]{1,}")
+            # [ ^.*]{1, }(\{{1}[ ^\[\{\(]{0, }.*[^ \}\]\)]{1, }\}{1})[ ^.*]{1, }
+            template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\[{1}.{0,}\]{1})[^.*\}\]\)\{\(\[]{0,}")
+
+            truth = [truth for truth in old_str if truth in "}]){(["]
+            if truth != []:
+              print("One symbol '}]){([' found")
+              exit()
+
+            old_str = (re.search(template, self.var_string).group(0)[1:-1])
+            result = self.var_string.replace(str(old_str), "")
+            self.var_string = result
+            # template = old_str = result = None
+            print(self.var_string)
+          if len(self.stack) == 0:
+            break
     else:
-      ...
+      exit()
 
   def push(self):
     self.stack.append(...)
@@ -20,4 +81,6 @@ class Stack:
   def size(self):
     ...
 
-
+s = "(((([{55 }]))"
+g = Stack(s)
+g.isEmpty()
