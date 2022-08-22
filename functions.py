7febcc84e9c0f55df43546f66e55d3e28a41dd2a
Tryd0g0lik <work80@mail.ru>
2022-08-22 13:08:57 +0700

    first commit

--- functions.py
+++ functions.py
@@ -4,77 +4,101 @@ class Stack:
   def __init__(self, var_string : str):
     self.var_string = var_string
     self.stack_front = []
-    self.stack_back = []
-    self.verify = True
+    self.stack_empty = []
+
 
   def isEmpty(self):
+    verify = True
     symbol : str
     time_symbol : str
-
+    bisic_leng_text = len(self.var_string)
     if len(self.var_string) != 0:
       front_path_string = ''.join(reversed(self.var_string[0 : len(self.var_string)//2]))
-      back_path_string = (self.var_string[len(self.var_string)//2 : ])
+      i = 0
 
-      for symbol in front_path_string:
-        if symbol in "{([":
-          self.stack_front.append(symbol)
+      while verify:
+        for symbol in front_path_string:
+          # if symbol in "{([":
+          #   self.stack_front.append(symbol)
 
-        for time_symbol in "}])":
+          for time_symbol in "}])":
 
-          if symbol == "{" and time_symbol == "}" and symbol in self.var_string and time_symbol in self.var_string:
-            template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\{{1}[\s0-9a-zA-Zа-яёА-ЯЁ]{0,}\}{1})[^.*\}\]\)\{\(\[]{0,}")
+            if symbol == "{" and time_symbol == "}" and symbol in self.var_string and time_symbol in self.var_string:
+              try:
+                template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\{{1}[\s0-9a-zA-Zа-яёА-ЯЁ]{0,}\}{1})[^.*\}\]\)\{\(\[]{0,}")
+                old_str = (re.search(template, self.var_string).group(0)) #[1:-1]
+              except AttributeError:
+                continue
 
-            old_str = (re.search(template, self.var_string).group(0)) #[1:-1]
-            truth = [truth for truth in old_str if truth in "}]){(["][1 : -1]
-            if truth != []:
-              print("One symbol '}]){([' found")
-              exit()
+              truth = [truth for truth in old_str if truth in "}]){(["][1 : -1]
+              if truth != []:
+                print("One symbol '}]){([' found")
+                exit()
+              if len(old_str) == 0:
+                self.stack_empty.append(old_str)
 
-            result = self.var_string.replace(str(old_str), "")
-            self.var_string = result
-            print(self.var_string)
-            time_symbol = ""
+              result = self.var_string.replace(str(old_str), "")
+              self.var_string = result
+              print(self.var_string)
+              time_symbol = ""
+              break
 
-          if symbol == "(" and time_symbol == ")" and symbol in self.var_string and time_symbol in self.var_string:
-            template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\({1}[\s0-9a-zA-Zа-яёА-ЯЁ]{0,}\){1})[^.*\}\]\)\{\(\[]{0,}")
+            if symbol == "(" and time_symbol == ")" and symbol in self.var_string and time_symbol in self.var_string:
+              try:
+                template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\({1}[\s0-9a-zA-Zа-яёА-ЯЁ]{0,}\){1})[^.*\}\]\)\{\(\[]{0,}")
+                old_str = re.search(template, self.var_string).group(0) #[1:-1])
+              except AttributeError:
+                continue
 
-            old_str = re.search(template, self.var_string).group(0) #[1:-1])
-            truth = [truth for truth in old_str if truth in "}]){(["][1:-1]
-            if truth != []:
-              print("One symbol '}]){([' found")
-              exit()
+              truth = [truth for truth in old_str if truth in "}]){(["][1:-1]
+              if truth != []:
+                print("One symbol '}]){([' found")
+                exit()
 
+              if len(old_str) == 0:
+                self.stack_empty.append(old_str)
 
-            result = self.var_string.replace(str(old_str), "")
-            self.var_string = result
-            print(self.var_string)
-            time_symbol = ""
+              result = self.var_string.replace(str(old_str), "")
+              self.var_string = result
+              print(self.var_string)
+              time_symbol = ""
+              break
 
-          if symbol == "[" and time_symbol == "]" and symbol in self.var_string and time_symbol in self.var_string:
-            template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\[{1}[\s0-9a-zA-Zа-яёА-ЯЁ]{0,}\]{1})[^.*\}\]\)\{\(\[]{0,}")
+            if symbol == "[" and time_symbol == "]" and symbol in self.var_string and time_symbol in self.var_string:
+              try:
+                template = re.compile("[^.*\}\]\)\{\(\[]{0,}(\[{1}[\s0-9a-zA-Zа-яёА-ЯЁ]{0,}\]{1})[^.*\}\]\)\{\(\[]{0,}")
+                old_str = re.search(template, self.var_string).group(0) #[1:-1])
+              except AttributeError:
+                continue
 
-            old_str = re.search(template, self.var_string).group(0) #[1:-1])
-            truth = [truth for truth in old_str if truth in "}]){(["][1:-1]
-            if truth != []:
-              print("One symbol '}]){([' found")
-              exit()
+              truth = [truth for truth in old_str if truth in "}]){(["][1:-1]
+              if truth != []:
+                print("One symbol '}]){([' found")
+                exit()
 
+              if len(old_str) == 0:
+                self.stack_empty.append(old_str)
 
-            result = self.var_string.replace(str(old_str), "")
-            self.var_string = result
-            print(self.var_string)
-            time_symbol = ""
+              result = self.var_string.replace(str(old_str), "")
+              self.var_string = result
+              print(self.var_string)
+              time_symbol = ""
+              break
+          continue
 
-      if len(self.var_string) == 0:
-        print("All symbol '}]){([' paired")
-        exit()
-
-      elif len(self.var_string) != 0:
-        print(f"ramins {self.var_string}, not find the paired ")
-      exit()
+        if len(self.var_string) == 0:
+          print("All symbol '}]){([' paired", f"Empty {self.stack_empty} and number: {len(self.stack_empty)}")
+          exit()
+        i +=1
+        if i >= bisic_leng_text - 1:
+          print(f"ramins {self.var_string}, not find the paired", f"Empty {self.stack_empty} and number: {len(self.stack_empty)}")
+          verify = False
+        # elif len(self.var_string) != 0:
+        #   print(f"ramins {self.var_string}, not find the paired ")
+        #   exit()
     else:
       if len(self.var_string) == 0:
-        print(f"ramins {self.var_string}, not find the paired")
+        print(f"ramins {self.var_string}, not find the paired", f"Empty {self.stack_empty} and number: {len(self.stack_empty)}")
       exit()
 
 
@@ -91,7 +115,7 @@ class Stack:
   def size(self):
     ...
 
-s = "(((([{55 }]))))"
-# s = "((((55 ))"
+# s = "([]((([{}])){dds}))"
+s = "[[{()}]"
 g = Stack(s)
 g.isEmpty()
